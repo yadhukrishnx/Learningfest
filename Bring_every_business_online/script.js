8039ce5c1efedd13b33bf5ee8745d56191e6807f
Yadhukrishna <145049379+yadhukrishnx@users.noreply.github.com>
2023-11-06 00:19:17 +0530

    Add files via upload

---
+++ Bring_every_business_online/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+      interval: 1000 // 3000 milliseconds (3 seconds)
+    });
+  });
+
+
+<script>
+    $(document).ready(function(){
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
