21cd3a0f2fa8872f8b5b635d7e9d14e85c719f0e
Yadhukrishna <145049379+yadhukrishnx@users.noreply.github.com>
2023-11-07 16:32:33 +0530

    Add files via upload
    v2

---
+++ Bring_every_business_online/script.js
@@ -0,0 +1,19 @@
+
+
+
+    $(document).ready(function(){
+        $('.dropdown-toggle').dropdown();
+    });
+
+  function color(id){
+    
+    document.getElementById(id).style.background="rgba(233, 127, 7, 0.37)";
+  }
+  function decolor(){
+    
+    document.getElementById("topnav").style.background=" rgba(77, 139, 64, 0.27)";
+  }
+  function at(){
+    confirm("Confirm to Contact ? ")
+   
+  }
